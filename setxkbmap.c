6b195148c00b990923ad5de4d021d4eeacbede68
Daniel Stone <daniel@fooishbar.org>
2006-03-10 09:09:12 +0000

    Bump to 1.0.2; release. Initialise vd.variant and vd.variant to NULL when failing to get the active     RMLVO components from the server.

--- setxkbmap.c
+++ setxkbmap.c
@@ -1,4 +1,4 @@
-/* $XdotOrg$ */
+/* $XdotOrg: app/setxkbmap/setxkbmap.c,v 1.2 2004/04/23 19:54:37 eich Exp $ */
 /************************************************************
  Copyright (c) 1996 by Silicon Graphics Computer Systems, Inc.
 
@@ -468,6 +468,8 @@ char *			tmp= NULL;
         tmp = DFLT_XKB_RULES_FILE;
         vd.model = DFLT_XKB_MODEL;
         vd.layout = DFLT_XKB_LAYOUT;
+        vd.variant = NULL;
+        vd.options = NULL;
         VMSG3(3,"Use defaults: rules - '%s' model - '%s' layout - '%s'\n",
                 tmp, vd.model, vd.layout);
     }
